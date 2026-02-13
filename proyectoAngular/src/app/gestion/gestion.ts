741b67c09ed0ac5185e1bdf6da2c57a6ff65c2e2
jordiPortatil <jordimarset22@gmail.com>
2026-02-13 18:37:32 +0100

    clase viernes final

--- proyectoAngular/src/app/gestion/gestion.ts
+++ proyectoAngular/src/app/gestion/gestion.ts
@@ -2,10 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Pizza } from '../interfaces/pizza';
 import { PizzaService } from '../services/pizza.service';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-gestion',
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule, CommonModule],
   templateUrl: './gestion.html',
   styleUrl: './gestion.css',
 })
@@ -23,10 +25,32 @@ export class Gestion implements OnInit {
 
 
   constructor(private pizzaService: PizzaService){}
+  ngOnInit() {
+    this.cargarPizzas();
+  }
+
+  cargarPizzas(){
+    this.listaPizzas = this.pizzaService.getPizzas();
+  }
 
   addPizza(){
     this.newPizza.id = Date.now();
     this.pizzaService.addPizza({...this.newPizza});
     this.newPizza = {id:0 , nombre: "", desc: "", precio: 0, imagen: ""}
+    this.cargarPizzas();
+  }
+
+  deletePizza(id: number){
+    this.pizzaService.deletePizza(id);
+    this.cargarPizzas();
+  }
+
+  changeImage(fileInput: HTMLInputElement){
+    if(!fileInput.files || fileInput.files.length === 0) return;
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      this.newPizza.imagen = reader.result as string;
+    };
+    reader.readAsDataURL(fileInput.files[0]);
   }
 }
